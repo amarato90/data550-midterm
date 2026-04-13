3baa95aaea94a8f441a993fb71caa76c5c91b887
Victoria Chang <victoria.chang@emory.edu>
2026-04-13 14:52:29 -0400

    added config.yml and updated code/03_render_report.R and report.Rmd to allow for parameterization

--- code/03_render_report.R
+++ code/03_render_report.R
@@ -1,10 +1,21 @@
 here::i_am("code/03_render_report.R")
 
+WHICH_CONFIG <- Sys.getenv("WHICH_CONFIG") 
+config_list <- config::get(
+  config = WHICH_CONFIG
+)
+
+
 library(rmarkdown)
+report_filename <- paste0(
+  "hiv_report_config_",
+  WHICH_CONFIG,
+  ".html"
+)
 
 # rendering a report in production mode
-rmarkdown::render(
-  "report.Rmd",
-  output_file = "report.html")
+render(
+  "hiv_report.Rmd",
+  output_file = report_filename)
 
 
